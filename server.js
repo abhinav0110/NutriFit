144ff637a66bd183942ecc1f3dfdc9134246f78b
David Bang <davidbang@Davids-MacBook-Pro-3.local>
2016-03-05 18:15:47 -0500

    modified server

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var apiai = require('apiai');
 var express = require('express');
 var app = express();
 // API.AI Connector (Client Key, Sub Key)
-var api = apiai("3a70592383834529b940f1862e5bcc0b ", "c115821f-c02a-4455-ab87-6734db7054c4 ");
+var api = apiai("7b0c3ff1e4584aba9b08728e35debff6", 
+"68d7a43e-c1bb-4ded-a97f-a6c51a2ffbec");
 
 
 var bodyParser = require('body-parser');
@@ -26,14 +27,22 @@ app.post('/', function (req, res) {
 	request.on('response', function(response) {
     	console.log(response);
     	var objects = response.result.parameters;
-    	if(objects.RepBaseWorkouts != undefined){
-    		textResponse.activity = objects.RepBaseWorkouts;
-    		textResponse.number = objects.number;
+    	if(objects.Food_eaten != undefined){
+    		textResponse.activity = objects.Food_eaten;
+ 		if (objects.number != "") {
+    			textResponse.number = objects.number;
+ 		}
+		else {
+			textResponse.number = "1";
+		}
     	}
-    	else if(objects.TimeBaseWorkouts != undefined){
-    		textResponse.activity = objects.TimeBaseWorkouts;
-    		textResponse.duration = objects.duration;
-    	}
+    	else if(objects.Food_option != undefined){
+    		textResponse.activity = objects.Food_option;
+    		textResponse.text = objects.Food;
+	}
+	else if(objects.Food_toeat != undefined){
+		textResponse.activity = objects.Food_toeat;
+	}
 		textResponse.intent = response.result.action;
 		console.log("send: " +response.result.action);
 	});
